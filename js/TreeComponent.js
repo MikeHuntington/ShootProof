cabd9a8d7eeb9dd4ed5abb4f0d67645155811617
Michael Huntington <mellomike3000@gmail.com>
2019-11-29 19:40:49 -0500

    adjusting application so that it can load without running a local server

--- js/TreeComponent.js
+++ js/TreeComponent.js
@@ -1,21 +1,63 @@
 var TreeComponent = (function () {
     // private
     let componentDOM;
-    let rawComponentData = [];
     let formattedComponentData = [];
-
-    // Load in our json data
-    const loadJSON = (callback) => {
-        let xobj = new XMLHttpRequest();
-        xobj.overrideMimeType("application/json");
-        xobj.open('GET', 'data/testdata.json', true);
-        xobj.onreadystatechange = () => {
-            if (xobj.readyState === 4 && xobj.status === 200) {
-                callback(xobj.responseText);
-            }
-        };
-        xobj.send(null);
-    }
+    let rawComponentData = [
+        {
+          "id": 0,
+          "name": "Node 0",
+          "thumbnail": {
+            "description": "Random Picture",
+            "href": "https://picsum.photos/200/200/?random&a"
+          },
+          "parent": null
+        },
+        {
+          "id": 1,
+          "name": "Node 1",
+          "thumbnail": {
+            "description": "Another Random Picture",
+            "href": "https://picsum.photos/200/200/?random&b"
+          },
+          "parent": 0
+        },
+        {
+          "id": 2,
+          "name": "Node 2",
+          "thumbnail": {
+            "description": "A Picture Is Random",
+            "href": "https://picsum.photos/200/200/?random&c"
+          },
+          "parent": null
+        },
+        {
+          "id": 3,
+          "name": "Node 3",
+          "thumbnail": {
+            "description": "Picture, Random",
+            "href": "https://picsum.photos/200/200/?random&d"
+          },
+          "parent": 1
+        },
+        {
+          "id": 4,
+          "name": "Node 4",
+          "thumbnail": {
+            "description": "rand(pix)",
+            "href": "https://picsum.photos/200/200/?random&e"
+          },
+          "parent": 5
+        },
+        {
+          "id": 5,
+          "name": "Node 5",
+          "thumbnail": {
+            "description": "pickPix <- rand",
+            "href": "https://picsum.photos/200/200/?random&f"
+          },
+          "parent": null
+        }
+    ];
 
     // Format the Tree Component data
     function formatData() {
@@ -148,17 +190,13 @@ var TreeComponent = (function () {
 
     // public
     return {
-        init: function(e) {
+        init: function(treeElement) {
             // Get a reference to our component's DOM
-            componentDOM = document.getElementById("treeComponent");
+            componentDOM = document.getElementById(treeElement);
 
-            // load json data
-            loadJSON(function(response) {
-                rawComponentData = JSON.parse(response);
-                formatData();
-                drawComponentNodes();
-                addClickListeners();
-            });
+            formatData();
+            drawComponentNodes();
+            addClickListeners();
         }
     }
 
